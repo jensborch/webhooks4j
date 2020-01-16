cd2119925752d2ea5bc5a7ec5c745b3560de5ff1
Jens Borch Christiansen <jens.borch@gmail.com>
2020-01-16 20:20:48 +0100

    Added client producer

---
+++ quarkus/src/main/java/dk/jensborch/webhooks/ClientProducer.java
@@ -0,0 +1,24 @@
+package dk.jensborch.webhooks;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+import dk.jensborch.webhooks.consumer.Consumer;
+import dk.jensborch.webhooks.publisher.Publisher;
+
+/**
+ *
+ */
+@Dependent
+public class ClientProducer {
+
+    @Produces
+    @Consumer
+    @Publisher
+    public Client getClient() {
+        return ClientBuilder.newClient();
+    }
+
+}
