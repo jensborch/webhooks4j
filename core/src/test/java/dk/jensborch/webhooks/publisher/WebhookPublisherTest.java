7a5f017053470283aed952ff42378290c2170940
Jens Borch Christiansen <jens.borch@gmail.com>
2020-02-05 19:18:46 +0100

    Added tests

--- core/src/test/java/dk/jensborch/webhooks/publisher/WebhookPublisherTest.java
+++ core/src/test/java/dk/jensborch/webhooks/publisher/WebhookPublisherTest.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
@@ -17,6 +18,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import dk.jensborch.webhooks.Webhook;
+import dk.jensborch.webhooks.WebhookError;
 import dk.jensborch.webhooks.WebhookEvent;
 import dk.jensborch.webhooks.repository.WebhookRepository;
 import dk.jensborch.webhooks.status.StatusRepository;
@@ -45,6 +47,9 @@ public class WebhookPublisherTest {
     @Mock
     private StatusRepository statusRepo;
 
+    @Mock
+    private Response response;
+
     @InjectMocks
     private WebhookPublisher publisher;
 
@@ -54,7 +59,6 @@ public class WebhookPublisherTest {
         Invocation.Builder builder = mock(Invocation.Builder.class);
         lenient().when(target.request(eq(MediaType.APPLICATION_JSON))).thenReturn(builder);
         lenient().when(client.target(any(URI.class))).thenReturn(target);
-        Response response = mock(Response.class);
         lenient().when(response.getStatusInfo()).thenReturn(Response.Status.ACCEPTED);
         lenient().when(builder.post(any(Entity.class))).thenReturn(response);
         lenient().when(statusRepo.save(any())).then(returnsFirstArg());
@@ -68,6 +72,30 @@ public class WebhookPublisherTest {
 
     @Test
     public void testPublish() throws Exception {
+        Set<Webhook> hooks = new HashSet<>();
+        hooks.add(new Webhook(new URI("http://test.dk"), new URI("http://test.dk"), new HashSet<>()));
+        when(repo.list(TOPIC)).thenReturn(hooks);
+        publisher.publish(new WebhookEvent(TOPIC, new HashMap<>()));
+        verify(repo, times(1)).list(eq(TOPIC));
+        verify(statusRepo, times(2)).save(any());
+    }
+
+    @Test
+    public void testPublishProcessingException() throws Exception {
+        when(response.getStatusInfo()).thenReturn(Response.Status.BAD_REQUEST);
+        when(response.readEntity(any(Class.class))).thenThrow(new ProcessingException("test"));
+        Set<Webhook> hooks = new HashSet<>();
+        hooks.add(new Webhook(new URI("http://test.dk"), new URI("http://test.dk"), new HashSet<>()));
+        when(repo.list(TOPIC)).thenReturn(hooks);
+        publisher.publish(new WebhookEvent(TOPIC, new HashMap<>()));
+        verify(repo, times(1)).list(eq(TOPIC));
+        verify(statusRepo, times(2)).save(any());
+    }
+
+    @Test
+    public void testPublishFailure() throws Exception {
+        when(response.getStatusInfo()).thenReturn(Response.Status.BAD_REQUEST);
+        when(response.readEntity(any(Class.class))).thenReturn(new WebhookError(WebhookError.Code.UNKNOWN_PUBLISHER, "test"));
         Set<Webhook> hooks = new HashSet<>();
         hooks.add(new Webhook(new URI("http://test.dk"), new URI("http://test.dk"), new HashSet<>()));
         when(repo.list(TOPIC)).thenReturn(hooks);
