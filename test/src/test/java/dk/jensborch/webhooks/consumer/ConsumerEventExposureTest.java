5861fea6974c1c69d4cd1d35cd2adc3f06e029bb
Jens Borch Christiansen <jens.borch@gmail.com>
2020-02-07 19:59:26 +0100

    Updated tests

--- test/src/test/java/dk/jensborch/webhooks/consumer/ConsumerEventExposureTest.java
+++ test/src/test/java/dk/jensborch/webhooks/consumer/ConsumerEventExposureTest.java
@@ -1,18 +1,19 @@
-package dk.jensborch.webhooks.publisher;
+package dk.jensborch.webhooks.consumer;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 
 import java.net.URI;
+import java.util.HashMap;
 
-import dk.jensborch.webhooks.Webhook;
+import dk.jensborch.webhooks.*;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
@@ -20,14 +21,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 /**
- * Integration test for {@link PublisherWebhookExposure}.
+ * Integration test for
+ * {@link dk.jensborch.webhooks.consumer.ConsumerEventExposur}
  */
 @QuarkusTest
 @TestMethodOrder(OrderAnnotation.class)
-public class PublisherWebhookExposureTest {
+public class ConsumerEventExposureTest {
 
-    private static final String TEST_TOPIC = PublisherWebhookExposureTest.class.getName();
+    private static final String TEST_TOPIC = ConsumerEventExposureTest.class.getName();
+    private static Webhook webhook;
     private RequestSpecification spec;
+
+    @BeforeAll
+    public static void setUpClass() throws Exception {
+        webhook = new Webhook(new URI("http://localhost:8081/publisher-webhooks"), new URI("http://localhost:8081/consumer-events"), TEST_TOPIC);
+    }
 
     @BeforeEach
     public void setUp() throws Exception {
@@ -42,12 +50,12 @@ public class PublisherWebhookExposureTest {
 
     @Test
     @Order(1)
-    public void testCreateWebhook() throws Exception {
+    public void testRegisterWebhook() {
         String location = given()
                 .spec(spec)
                 .when()
-                .body(new Webhook(new URI("http://localhost:8081/publisher-webhooks"), new URI("http://localhost:8081/consumer-events"), TEST_TOPIC))
-                .post("publisher-webhooks")
+                .body(webhook)
+                .post("consumer-webhooks")
                 .then()
                 .statusCode(201)
                 .extract()
@@ -59,19 +67,18 @@ public class PublisherWebhookExposureTest {
                 .get(location)
                 .then()
                 .statusCode(200)
-                .body("topics[0]", is(TEST_TOPIC));
-    }
-
-    @Test
-    @Order(1)
-    public void testListWebhooks() throws Exception {
-        given()
-                .spec(spec)
-                .when()
-                .get("publisher-webhooks")
-                .then()
-                .statusCode(200)
                 .body("size()", greaterThan(1));
     }
 
+    @Test
+    @Order(2)
+    public void testPublishEvent() {
+        given()
+                .spec(spec)
+                .when()
+                .body(new WebhookEvent(webhook.getId(), TEST_TOPIC, new HashMap<>()))
+                .post("consumer-events")
+                .then()
+                .statusCode(201);
+    }
 }
