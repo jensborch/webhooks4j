773a75e3315b48c58b3dccfc3f39505704e802ee
Jens Borch Christiansen <jens.borch@gmail.com>
2020-03-06 12:57:19 +0100

    Fixed test

--- core/src/test/java/dk/jensborch/webhooks/WebhhookEventStatusTest.java
+++ core/src/test/java/dk/jensborch/webhooks/WebhhookEventStatusTest.java
@@ -1,19 +1,17 @@
 package dk.jensborch.webhooks;
 
-import dk.jensborch.webhooks.WebhookEventStatus;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.URI;
 import java.util.HashMap;
+import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 
-import dk.jensborch.webhooks.Webhook;
-import dk.jensborch.webhooks.WebhookEvent;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; 
 
 /**
  * Test for {@link WebhookEventStatus}.
@@ -34,7 +32,7 @@ public class WebhhookEventStatusTest {
     @Test
     public void testDone() {
         WebhookEventStatus result = status.done(true);
-        assertEquals(WebhookEventStatus.Status.SUCCESS, status.getStatus());
+        assertEquals(WebhookEventStatus.Status.SUCCESS, result.getStatus());
     }
 
     @Test
@@ -43,10 +41,11 @@ public class WebhhookEventStatusTest {
     }
 
     @Test
-    public void testCompareTo() {
+    public void testCompareTo() throws Exception {
+        TimeUnit.SECONDS.sleep(1);
         WebhookEventStatus status2 = new WebhookEventStatus(new WebhookEvent(webhook.getId(), "test", new HashMap<>()), webhook.getId());
         assertTrue(status.compareTo(status2) > 0);
-        TreeSet<WebhookEventStatus> list = new TreeSet<>();
+        SortedSet<WebhookEventStatus> list = new TreeSet<>();
         list.add(status);
         list.add(status2);
         assertEquals(status2, list.stream().findFirst().get());
