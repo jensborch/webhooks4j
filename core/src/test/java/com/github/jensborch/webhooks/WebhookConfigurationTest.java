3fc8fcf1efd8c7d6089d891f64a04f22418ff83e
Jens Borch Christiansen <jens.borch@gmail.com>
2020-09-01 16:36:03 +0200

    Added test

---
+++ core/src/test/java/com/github/jensborch/webhooks/WebhookConfigurationTest.java
@@ -0,0 +1,46 @@
+package com.github.jensborch.webhooks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link WebhookConfiguration}.
+ */
+class WebhookConfigurationTest {
+
+    @Test
+    void testSyncFrom() throws Exception {
+        WebhookConfiguration configuration = new WebhookConfiguration() {
+            @Override
+            public long getTimeToLive() {
+                return 1;
+            }
+
+            @Override
+            public TimeUnit getTimeToLiveTimeUnit() {
+                return TimeUnit.MINUTES;
+            }
+
+            @Override
+            public long getSyncOffset() {
+                return 1;
+            }
+
+            @Override
+            public TemporalUnit getSyncOffsetUnit() {
+                return ChronoUnit.SECONDS;
+            }
+        };
+        ZonedDateTime now = ZonedDateTime.now();
+        Webhook webhook = new Webhook(new URI("http://pub.dk"), new URI("http://sub.dk"), "test").updated(now);
+        assertEquals(now.minusSeconds(1), configuration.syncFrom(webhook));
+    }
+
+}
