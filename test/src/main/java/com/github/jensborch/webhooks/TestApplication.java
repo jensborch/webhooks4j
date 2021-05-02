7532a97eb2c10f9deef7da469470df70900bee97
Jens Borch Christiansen <jens.borch@gmail.com>
2021-05-02 16:45:57 +0200

    Updated Quarkus

--- test/src/main/java/com/github/jensborch/webhooks/TestApplication.java
+++ test/src/main/java/com/github/jensborch/webhooks/TestApplication.java
@@ -8,6 +8,8 @@ import javax.ws.rs.core.Application;
 
 import com.github.jensborch.webhooks.subscriber.SubscriberEventExposure;
 import com.github.jensborch.webhooks.subscriber.SubscriberWebhookExposure;
+import com.github.jensborch.webhooks.exceptionmappers.ConstraintViolationExceptionMapper;
+import com.github.jensborch.webhooks.exceptionmappers.WebhookExceptionMapper;
 import com.github.jensborch.webhooks.publisher.PublisherEventExposure;
 import com.github.jensborch.webhooks.publisher.PublisherWebhookExposure;
 
@@ -19,6 +21,9 @@ public class TestApplication extends Application {
     @Override
     public Set<Class<?>> getClasses() {
         return Arrays.stream(new Class<?>[]{
+            WebhookExceptionMapper.class,
+            ConstraintViolationExceptionMapper.class,
+            DefaultExceptionMapper.class,
             SubscriberEventExposure.class,
             SubscriberWebhookExposure.class,
             PublisherEventExposure.class,
